a8cb6552f2409d17ff255706d6a452c5864ada17
VivaEjay <vivaciousejay@gmail.com>
2022-09-14 17:25:09 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,19 @@
-not sure what goes in here
+#include <main.h>
+/**
+ * main - entry point
+ * Description: prints '_putchar \n'
+ * Return: Always 0
+ */
+int main(void)
+{
+	char str[] = "_putchar\n";
+	int i = 0;
+	while (str[i] !='\0')
+	{
+		char c = str[i];
+		
+		_putchar(c);
+		i++;
+	}
+	return (0);
+}
